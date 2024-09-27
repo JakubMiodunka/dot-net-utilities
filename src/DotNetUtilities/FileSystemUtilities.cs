9d07e02ea8e0994a14978fdc762c061bd4942ae5
JakubMiodunka <miodunkajakub@gmail.com>
2024-09-27 15:21:09 +0200

    Initial commit. Moving locally stored files to repository.

---
+++ src/DotNetUtilities/FileSystemUtilities.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace DotNetUtilities
+{
+    /// <summary>
+    /// Set of utilities related to file system.
+    /// </summary>
+    public static class FileSystemUtilities
+    {
+        /// <summary>
+        /// Checks if specified file system entry exists and is a directory.
+        /// If either of above cases would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="shallExist">
+        /// Specifies if provided file system entry shall exists within file system as directory.
+        /// Useful, when there is a need to check if specified path is not already
+        /// in use by other directory in file system.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown, when at least one argument will be considered as invalid.
+        /// </exception>
+        /// <exception cref="DirectoryNotFoundException">
+        /// Thrown, when provided file system entry does not exist in file system as directory.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when provided file system entry exists within file system as directory but shall not.
+        /// </exception>
+        public static void ValidateDirectory(string path, bool shallExist = true)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                string argumentName = nameof(path);
+                string errorMessage = $"Provided path is invalid: {path}";
+                throw new ArgumentException(argumentName, errorMessage);
+            }
+
+            if (shallExist)
+            {
+                if (!Directory.Exists(path))
+                {
+                    if (File.Exists(path))
+                    {
+                        string errorMessage = $"Given entry is a file: {path}";
+                        throw new DirectoryNotFoundException(errorMessage);
+                    }
+                    else
+                    {
+                        string errorMessage = $"Directory does not exist: {path}";
+                        throw new DirectoryNotFoundException(errorMessage);
+                    }
+                }
+            }
+            else
+            {
+                if (Directory.Exists(path))
+                {
+                    string errorMessage = $"Directory already exist: {path}";
+                    throw new IOException(errorMessage);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if extension of specified file system entry is equal to expected one.
+        /// If validation condition would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <remarks>
+        /// Check is based entirely on provided path - method does not check
+        /// if specified entry exists within file system.
+        /// </remarks>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="validExtensions">
+        /// Collection of extensions, which shall be considered as valid for validated file.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown, when at least one argument will be considered as invalid.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when specified file system entry has invalid extension.
+        /// </exception>
+        public static void ValidateExtension(string path, IEnumerable<string> validExtensions)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                string argumentName = nameof(path);
+                string errorMessage = $"Provided path is invalid: {path}";
+                throw new ArgumentException(argumentName, errorMessage);
+            }
+
+            if (validExtensions is null)
+            {
+                string argumentName = nameof(validExtensions);
+                const string ErrorMessage = "Provided extensions collection is null reference:";
+                throw new ArgumentNullException(argumentName, ErrorMessage);
+            }
+
+            if (validExtensions.Any(string.IsNullOrWhiteSpace))
+            {
+                string argumentName = nameof(path);
+                const string ErrorMessage = "Provided extensions collection contains invalid extension:";
+                throw new ArgumentException(argumentName, ErrorMessage);
+            }
+
+            string actualExtension = Path.GetExtension(path);
+
+            if (!validExtensions.Contains(actualExtension))
+            {
+                string errorMessage = $"Invalid extension: {actualExtension}";
+                throw new IOException(errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Checks if extension of specified file system entry is equal to expected one.
+        /// If validation condition would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <remarks>
+        /// Check is based entirely on provided path - method does not check
+        /// if specified entry exists within file system.
+        /// </remarks>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="validExtension">
+        /// Extension, which shall be considered as valid for validated file.
+        /// </param>
+        public static void ValidateExtension(string path, string validExtension)
+        {
+            string[] validExtensions = { validExtension };
+
+            ValidateExtension(path, validExtensions);
+        }
+
+        /// <summary>
+        /// Checks if specified file system entry exists, is a file and its extension
+        /// matches the expected one.
+        /// If either validation condition would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="shallExist">
+        /// Specifies if provided file system entry shall exists within file system as file.
+        /// Useful, when there is a need to check if specified path is not already
+        /// in use by other file in file system.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown, when at least one argument will be considered as invalid.
+        /// </exception>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown, when provided file system entry does not exist in file system as file.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, when provided file system entry exists within file system as file but shall not.
+        /// </exception>
+        public static void ValidateFile(string path, bool shallExist = true)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                string argumentName = nameof(path);
+                string errorMessage = $"Provided file path is invalid: {path}";
+                throw new ArgumentException(argumentName, errorMessage);
+            }
+
+            if (shallExist)
+            {
+                if (!File.Exists(path))
+                {
+                    if (Directory.Exists(path))
+                    {
+                        string errorMessage = $"Given entry is a directory: {path}";
+                        throw new FileNotFoundException(errorMessage, path);
+                    }
+                    else
+                    {
+                        string errorMessage = $"File does not exist: {path}";
+                        throw new FileNotFoundException(errorMessage, path);
+                    }
+                }
+            }
+            else
+            {
+                if (File.Exists(path))
+                {
+                    string errorMessage = $"File already exist: {path}";
+                    throw new IOException(errorMessage);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if specified file system entry exists, is a file and its extension
+        /// matches the expected one.
+        /// If either validation condition would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="validExtensions">
+        /// Collection of extensions, which shall be considered as valid for validated file.
+        /// </param>
+        /// <param name="shallExist">
+        /// Specifies if provided file system entry shall exists within file system as file.
+        /// Useful, when there is a need to check if specified path is not already
+        /// in use by other file in file system.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        /// Thrown, when at least one argument will be considered as invalid.
+        /// </exception>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown, when provided file system entry does not exist in file system as file.
+        /// </exception>
+        /// <exception cref="IOException">
+        /// Thrown, either when provided file system entry exists within file system but shall not.
+        /// </exception>
+        public static void ValidateFile(string path, IEnumerable<string> validExtensions, bool shallExist = true)
+        {
+            ValidateFile(path, shallExist);
+            ValidateExtension(path, validExtensions);
+        }
+
+        /// <summary>
+        /// Checks if specified file system entry exists, is a file and its extension
+        /// matches the expected one.
+        /// If either validation condition would not be fulfilled, according exception will be thrown.
+        /// </summary>
+        /// <param name="path">
+        /// Path to file system entry, which shall be validated.
+        /// </param>
+        /// <param name="validExtension">
+        /// Extension, which shall be conceited as valid for validated file.
+        /// </param>
+        /// <param name="shallExist">
+        /// Specifies if provided file system entry shall exists within file system as file.
+        /// Useful, when there is a need to check if specified path is not already
+        /// in use by other file in file system.
+        /// </param>
+        public static void ValidateFile(string path, string validExtension, bool shallExist = true)
+        {
+            string[] validExtensions = { validExtension };
+
+            ValidateFile(path, validExtensions, shallExist);
+        }
+    }
+}
---
+++ src/DotNetUtilities/XmlUtilities.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+
+namespace DotNetUtilities
+{
+    /// <summary>
+    /// Set of utilities related to XML format.
+    /// </summary>
+    public static class XmlUtilities
+    {
+        /// <summary>
+        /// Checks if given XML document matches specified schema.
+        /// </summary>
+        /// <param name="xmlDocument">
+        /// XML document, which shall be validated.
+        /// </param>
+        /// <param name="schemaPath">
+        /// Path to *.xsd schema, against which provided XML document shall be validated against.
+        /// </param>
+        /// <returns>
+        /// True if provided XML document matches provided *.xsd schema, false otherwise.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown, when at least one reference-type argument is a null reference.
+        /// </exception>
+        public static bool IsMatchingToSchema(XDocument xmlDocument, string schemaPath)
+        {
+            if (xmlDocument is null)
+            {
+                string argumentName = nameof(xmlDocument);
+                const string ErrorMessage = "Provided XML document is a null reference:";
+                throw new ArgumentNullException(argumentName, ErrorMessage);
+            }
+
+            FileSystemUtilities.ValidateFile(schemaPath, ".xsd");
+
+            var schemaSet = new XmlSchemaSet();
+            schemaSet.Add(null, schemaPath);
+
+            bool isDocumentValid = true;
+            xmlDocument.Validate(schemaSet, (sender, eventData) => isDocumentValid = false);
+
+            return isDocumentValid;
+        }
+
+        /// <summary>
+        /// Checks if given XML file matches specified schema.
+        /// </summary>
+        /// <param name="filePath">
+        /// Path to *.xml file, which shall be validated.
+        /// </param>
+        /// <param name="schemaPath">
+        /// Path to *.xsd schema, against which provided XML file shall be validated against.
+        /// </param>
+        /// <returns>
+        /// True if provided XML file matches provided *.xsd schema, false otherwise.
+        /// </returns>
+        public static bool IsMatchingToSchema(string filePath, string schemaPath)
+        {
+            FileSystemUtilities.ValidateFile(filePath, ".xml");
+
+            var xmlDocument = XDocument.Load(filePath);
+
+            return IsMatchingToSchema(xmlDocument, schemaPath);
+        }
+
+        /// <summary>
+        /// Checks if given XML document matches specified schema.
+        /// If validation will fail, according exception will be thrown.
+        /// </summary>
+        /// <param name="xmlDocument">
+        /// XML document, which shall be validated.
+        /// </param>
+        /// <param name="schemataPath">
+        /// Path to *.xsd schema, against which provided XML document shall be validated against.
+        /// </param>
+        /// <exception cref="FormatException">
+        /// Thrown, when provided XML document does not match specified schema.
+        /// </exception>
+        public static void ValidateXmlDocument(XDocument xmlDocument, string schemataPath)
+        {
+            if (!IsMatchingToSchema(xmlDocument, schemataPath))
+            {
+                const string ErrorMessage = "XML document does not match the schema:";
+                throw new FormatException(ErrorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Checks if given XML file matches specified schema.
+        /// If validation will fail, according exception will be thrown.
+        /// </summary>
+        /// <param name="filePath">
+        /// Path to *.xml file, which shall be validated.
+        /// </param>
+        /// <param name="schemaPath">
+        /// Path to *.xsd schema, against which provided XML file shall be validated against.
+        /// </param>
+        /// <exception cref="FormatException">
+        /// Thrown, when provided XML file does not match specified schema.
+        /// </exception>
+        public static void ValidateXmlFile(string filePath, string schemaPath)
+        {
+            if (!IsMatchingToSchema(filePath, schemaPath))
+            {
+                string errorMessage = $"XML file doesn't match the schema: {filePath}";
+                throw new FormatException(errorMessage);
+            }
+        }
+    }
+}
